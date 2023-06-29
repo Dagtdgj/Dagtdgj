4d4a740a92cc1d878fdb24a660c2a33df598ce20
Dagtdgj <128801213+Dagtdgj@users.noreply.github.com>
2023-06-29 12:47:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import telebot
 from datetime import datetime, timedelta
 
-bot = telebot.TeleBot('здесь пишем свой API токен')
+bot = telebot.TeleBot('тут мой API токен')
 
 users = {}
+
 
 @bot.message_handler(commands=["start"])
 def start_command(message):
@@ -15,54 +16,25 @@ def start_command(message):
 
     доп. информация/все команды - /help""")
 
+
 @bot.message_handler(commands=["help"])
 def start(m, res=False):
     bot.send_message(m.chat.id, """вот, что на данный момент я умею:
-    
+
     /mute - выдавать мут пользователю на 1 час (3600 сек)
     /ban - кикать человека из чата
     /top - выдавать топ - пользователей за сегодня (данная функция является BETA, поэтому может не работать)
     /new_update - Здесь будет список новых обновлений бота, который обязательно будет дополнятся, а так же список того, что скоро будет обновлено в боте.
-    
-    что бы воспользоваться командами, вы должны ответить на сообщение пользователя, которого хотите забанить или же выдать мут.
-    
-    ненужная информация - /dop""")
 
-@ bot.message_handler(commands=["dop"])
-def dop(m, res=False):
-    bot.send_message(m.chat.id, """Привет, человек! Если ты написал эту команду, скорее всего тебе либо интересно что она выдает, либо ты пришел немного почитать про меня)
-
-На самом деле я был написан на языке - Python. Если так интересно, то вот мой код - https://github.com/Dagtdgj/Dagtdgj
-
-Мой код на самом деле довольно длинный, но очень простой. Он написан в 111 строчки кода. Что бы написать такого бота, достаточно знать базовые знания Python, не больше)
-
-Я надеюсь эта информация дала вам хоть какие-то новые знания. Меня будут дорабатывать и добавлять новые команды/функции, а так я только BETA - версия.
-
-Приятно было пообщаться, до новых встреч!""")
+    что бы воспользоваться командами, вы должны ответить на сообщение пользователя, которого хотите забанить или же выдать мут.""")
 
 
-@bot.message_handler(commands=['mute'])
-def mute_user(message):
-    chat_id = message.chat.id
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        bot.restrict_chat_member(chat_id, user_id, until_date=3600) # на 1 час
-    else:
-        bot.reply_to(message, "Вы должны ответить на сообщение пользователя, которого хотите замутить.")
+@bot.message_handler(commands=["idea"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, """Приветствую! Вы попали в раздел "идея". Мы создали нашего нового телеграм-бота, в котором вы можете предложить идею для нашего Бота-модератора! Вы можете предложить новые команды и их функции, что то поменять, и т.п.. 
 
-@bot.message_handler(commands=['ban'])
-def ban_user(message):
-    chat_id = message.chat.id
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        bot.kick_chat_member(chat_id, user_id)
-    else:
-        bot.reply_to(message, "Вы должны ответить на сообщение пользователя, которого хотите забанить.")
+    Ждем ваших идей в нашем телеграм-боте!  @boostDlog_bot""")
 
-# обработчик события нового пользователя в чате@bot.message_handler(content_types=['new_chat_members'])
-def welcome_new_member(message):
-    for member in message.new_chat_members:
-        bot.send_message(message.chat.id, f"Привет, {member.first_name}! Добро пожаловать в чат!\nПросим вас представиться в чате, т.к. кто-то вас знает, а кто-то даже не имеет понятия кто в чате)\nблагодарю за понимание :)")
 
 @bot.message_handler(commands=['top'])
 def top_users(message):
@@ -81,29 +53,80 @@ def top_users(message):
     if top_users:
         message_text = 'Топ-3 пользователей за сегодня:\n'
         for i, user_data in enumerate(top_users):
-            message_text += f"{i+1}. {user_data[0]} - {user_data[1]} сообщений\n"
+            message_text += f"{i + 1}. {user_data[0]} - {user_data[1]} сообщений\n"
         bot.send_message(chat_id, message_text)
     else:
         bot.send_message(chat_id, 'На сегодня нет данных о сообщениях пользователей')
 
+
 @bot.message_handler(commands=["new_update"])
 def handle_start(message):
-        user_name = message.from_user.first_name
-        bot.send_message(message.chat.id,
-                         f"""Привет, {user_name}! Ты попал(а) в раздел 'новые обновления'. Здесь будет список новых обновлений бота, который обязательно будет дополнятся, а так же список того, что скоро будет обновлено в боте!
-     
-     
+    user_name = message.from_user.first_name
+    bot.send_message(message.chat.id,
+                     f"""Привет, {user_name}! Ты попал(а) в раздел 'новые обновления'. Здесь будет список новых обновлений бота, который обязательно будет дополнятся, а так же список того, что скоро будет обновлено в боте!
+
+
 Вышедшие обновления:         
-                                
+
 1. Наш новый Телеграм-бот для предложений/идей для нашего бота-модератора    -   @boostDlog_bot 
-    
+
 Скоро:
-                     
+
 1. Новая база данных для улучшаемой производительности бота и устранения ошибок + хранение данных команды /top
 
 1.1. Финальная версия команды /top
 
 2. Устранение бага команд, которые действуют на бота (этого не должно быть)""")
+
+
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.reply_to_message:
+        if message.text == '/ban':
+            if message.reply_to_message.from_user.id == bot.get_me().id:
+                bot.reply_to(message, "Нельзя использовать команды на самом боте")
+            elif message.reply_to_message.from_user.id == message.from_user.id:
+                bot.reply_to(message, "Нельзя использовать команду на себе")
+            else:
+                # Perform ban logic here
+                # Send confirmation message with buttons
+                keyboard = telebot.types.InlineKeyboardMarkup()
+                confirm_button = telebot.types.InlineKeyboardButton(text="Продолжить", callback_data="ban_confirm")
+                cancel_button = telebot.types.InlineKeyboardButton(text="Отмена", callback_data="ban_cancel")
+                keyboard.add(confirm_button, cancel_button)
+                bot.reply_to(message, "Вы уверены, что хотите забанить пользователя?", reply_markup=keyboard)
+        elif message.text == '/mute':
+            if message.reply_to_message.from_user.id == bot.get_me().id:
+                bot.reply_to(message, "Нельзя использовать команды на самом боте")
+            elif message.reply_to_message.from_user.id == message.from_user.id:
+                bot.reply_to(message, "Нельзя использовать команду на себе")
+            else:
+                # Perform mute logic here
+                # Send confirmation message with buttons
+                keyboard = telebot.types.InlineKeyboardMarkup()
+                confirm_button = telebot.types.InlineKeyboardButton(text="Продолжить", callback_data="mute_confirm")
+                cancel_button = telebot.types.InlineKeyboardButton(text="Отмена", callback_data="mute_cancel")
+                keyboard.add(confirm_button, cancel_button)
+                bot.reply_to(message, "Вы уверены, что хотите выдать мут пользователю?", reply_markup=keyboard)
+
+                @bot.message_handler(commands=['mute'])
+                def mute_user(message):
+                    chat_id = message.chat.id
+                    if message.reply_to_message:
+                        user_id = message.reply_to_message.from_user.id
+                        bot.restrict_chat_member(chat_id, user_id, until_date=3600)  # на 1 час
+                    else:
+                        bot.reply_to(message, "Вы должны ответить на сообщение пользователя, которого хотите замутить.")
+
+                @bot.message_handler(commands=['ban'])
+                def ban_user(message):
+                    chat_id = message.chat.id
+                    if message.reply_to_message:
+                        user_id = message.reply_to_message.from_user.id
+                        bot.kick_chat_member(chat_id, user_id)
+                    else:
+                        bot.reply_to(message, "Вы должны ответить на сообщение пользователя, которого хотите забанить.")
+
 
 try:
     bot.polling()
